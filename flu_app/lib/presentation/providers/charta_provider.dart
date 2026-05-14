71ccf3fa1fb41e2bcf21da55f1eb0ad50cd2b38c
Pixxie Jinx <leonrosaparlante1@gmail.com>
2026-05-14 16:31:24 +0200

    ebelmon: Creación del selector de colores

--- flu_app/lib/presentation/providers/charta_provider.dart
+++ flu_app/lib/presentation/providers/charta_provider.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/legacy.dart';
 
 
-final formNomenProvider = StateProvider((ref)=> '');
-final formColorProvider = StateProvider((ref)=> Colors.red);
+final formNomenProvider = StateProvider<String>((ref)=> '');
+final formColorProvider = StateProvider<Color>((ref)=> Colors.red);
 
