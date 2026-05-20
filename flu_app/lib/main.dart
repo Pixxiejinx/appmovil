3752e459825d59f087a0f3abec29ff9d9165a98a
Pixxie Jinx <leonrosaparlante1@gmail.com>
2026-05-20 02:08:08 +0200

    ebelmon: Vemos el mapa y ocultamos el token

--- flu_app/lib/main.dart
+++ flu_app/lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flu_app/config/config.dart';
+import 'package:flu_app/config/secret.example.dart';
 import 'package:flu_app/presentation/providers/providers.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 Future<void> main() async {
- WidgetsFlutterBinding.ensureInitialized();
- MapboxOptions.setAccessToken(
-  "pk.eyJ1IjoicGl4eGllamlueCIsImEiOiJjbXA1bGEydGEwaTk1MnJzOTcwenFsZ2hvIn0.Wt0r33AMvWaIUGncNP8Wyw"
- );
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  
+  MapboxOptions.setAccessToken(mapBoxAccessToken);
+
   runApp(
     const ProviderScope(
       child: MainApp()
@@ -16,18 +20,22 @@ Future<void> main() async {
     );
 }
 
+//Hola esto es una prueba de commit
 class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    
-    final tenebrisModusEst = ref.watch(estTenebrisModusProvider);
+
+    final tenebrisModus = ref.watch(estTenebrisModusProvider);
 
     return MaterialApp.router(
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(tenebrisModusEst: tenebrisModusEst, electusColor: Colors.pink.shade200).getTheme(),
+      theme: AppTheme(
+        electusColor:Colors.deepOrangeAccent.shade100,
+        tenebrisModusEst: tenebrisModus
+        ).getTheme(),
     );
   }
 }
--- flu_app/lib/presentation/screens/charta/charta_screen.dart
+++ flu_app/lib/presentation/screens/charta/charta_screen.dart
@@ -1,9 +1,6 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:flu_app/presentation/providers/charta_provider.dart';
+import 'package:flu_app/presentation/widgets/complere_form.dart';
 import 'package:flu_app/presentation/widgets/widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 class ChartaScreen extends StatefulWidget {
@@ -15,71 +12,53 @@ class ChartaScreen extends StatefulWidget {
 
 class _ChartaScreenState extends State<ChartaScreen> {
 
-  void _initiareCirleAnnotations(MapboxMap mapboxMap) {
-    // Aquí puedes agregar anotaciones o personalizar el mapa después de que se haya creado
+  CircleAnnotationManager? _circleAnnotationManager;
+  
+
+  void _initializeCiecleAnnotations(MapboxMap mapBoxMap){
+  
+    mapBoxMap.annotations.createCircleAnnotationManager().then((manager){
+      _circleAnnotationManager = manager;
+
+      _addVelRenovareMarker();
+    });
   }
 
-   void _aperireColorPicker(BuildContext context, WidgetRef ref) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Selecciona un color'),
-          content: SingleChildScrollView(
-            child: Wrap(
-              spacing: 8,
-              runSpacing: 8,
-              children: ComplereForm.paletteColorum.map((color) {
-                return GestureDetector(
-                  onTap: () {
-                    ref.read(formColorProvider.notifier).state = color;
-                    Navigator.of(context).pop();
-                  },
-                  child: Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: color,
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.white, width: 2),
-                    ),
-                  ),
-                );
-              }).toList(),
-            ),
-          ),
-        );
-      },
-    );
+  Future<void> _addVelRenovareMarker() async{
+    final manager = _circleAnnotationManager;
+
+    if(manager == null) return;
+
+    final situs = Position(-122.467895, 37.800126);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Mapas'),
+        title: const Text('Maps'),
       ),
       body: Stack(
         fit: StackFit.expand,
-        // ignore: prefer_const_literals_to_create_immutables
         children: [
           MapWidget(
-            key: ValueKey('main_mapa'),
+            key: const ValueKey('main_mapa'),
             cameraOptions: CameraOptions(
               center: Point(
-                coordinates: Position(-122.467895, 37.800126)
+                coordinates: Position(-122.467895, 37.800126
                 ),
-                zoom: 14.5,
+              ),
+              zoom: 14.5,
             ),
             styleUri: MapboxStyles.MAPBOX_STREETS,
-            onMapCreated: _initiareCirleAnnotations,
+            onMapCreated: _initializeCiecleAnnotations
           ),
-          Align(
+          const Align(
             alignment: Alignment.topRight,
-              child: Padding(
-                padding: EdgeInsets.all(12),
-                child: ComplereForm()
-              ),
+            child: Padding(padding: EdgeInsets.all(8.0),
+            child: ComplereForm(),
             ),
+          ),
         ],
       ),
     );
