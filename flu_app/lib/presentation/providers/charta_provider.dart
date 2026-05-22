b93d82780cebacbe880f7f40ac3b18a1bb538be3
Pixxie Jinx <leonrosaparlante1@gmail.com>
2026-05-22 15:28:13 +0200

    ebelmon: Pantalla de mapbox terminada

--- flu_app/lib/presentation/providers/charta_provider.dart
+++ flu_app/lib/presentation/providers/charta_provider.dart
@@ -1,18 +1,30 @@
+import 'package:flu_app/config/config.dart';
+import 'package:flu_app/services/charta_service.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/legacy.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../services/charta_service.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
-final Position initialisMarkerPosition = Position(-122.467895, 37.800126);
+
+final formNomenProvider = StateProvider((ref) => '');
 
 final formColorProvider = StateProvider<Color>((ref) => Colors.red);
-final formNomenProvider = StateProvider<String>((ref) => '');
+
 final markerPositumProvider = StateProvider<bool>((ref) => false);
-final coordsMarkerProvider = StateProvider<Position>((ref) => initialisMarkerPosition);
+
+final Position initialisMarkerPoistio = Position(-122.467895, 37.800126);
+final coordsMarkerProvider = StateProvider<Position>((ref) => initialisMarkerPoistio);
+
 final socketServiceProvider = Provider<ChartaService>((ref) {
-final service = ChartaService();
- 
+  final service = ChartaService();
+
   ref.onDispose(service.finire);
-  return service; 
+  
+  return service;
+});
+
+
+final aliiUsoresProvider = StreamProvider<List<Usor>>((ref) {
+  final service = ref.watch(socketServiceProvider);
+  return service.usoresStream;
 });
