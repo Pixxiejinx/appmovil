1df91aacea49ffaa92103ea550eac7e3ac480b29
Pixxie Jinx <leonrosaparlante1@gmail.com>
2026-05-24 16:57:28 +0200

    ebelmon: Petición http y mapeado de la respuesta

--- flu_app/lib/services/pokemon_service.dart
+++ flu_app/lib/services/pokemon_service.dart
@@ -1,10 +1,18 @@
 import 'package:dio/dio.dart';
+import 'package:flu_app/config/config.dart';
+import 'package:flu_app/mappers/pokemon_mapper.dart';
 
 class PokemonService{
   static getPokemon<String>(String pokemonId)async{
     final dio = Dio();
     try{
       final responsio = await dio.get('https://pokeapi.co/api/v2/pokemon/$pokemonId');
-    } catch (e){}
+
+      final pokemon = PokemonMapper.pokeApiPokemonToEntity(responsio.data);
+      return (pokemon, 'Data obtenida corréctamente');
+    } catch (e){
+
+      return (null, 'No se pudo obtener los datos');
+    }
   }
 }
